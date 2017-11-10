aac94f8eadd1ed820b6600f1d45387db15946874
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-11-10 23:42:15 +0300

    Hibernate util was created

---
+++ src/main/java/com/mikhadyuk/scholarshipcalculator/util/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.mikhadyuk.scholarshipcalculator.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration().configure();
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
